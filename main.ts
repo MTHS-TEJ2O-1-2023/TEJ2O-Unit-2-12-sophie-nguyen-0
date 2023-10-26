73e0fbb09cfaa22981759aef611cc0f3adcab9b1
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-26 13:12:17 -0400

    code

--- main.ts
+++ main.ts
@@ -2,7 +2,42 @@
  *
  * Created by: sophie
  * Created on: oct 2023
- * This program ...
+ * This program detects distance with sonar and lights up LEDs.
 */
 
-basic.showString('Hello, World!')
+// variables
+let distance: number = 0
+let neopixelStrip: neopixel.Strip = null
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Duck)
+
+// cleanup
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+
+// find distance and light up LED's depending on distance
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+
+    distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+    basic.showNumber(distance)
+    basic.showIcon(IconNames.StickFigure)
+
+    if (distance < 10) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    } else {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    }
+})
